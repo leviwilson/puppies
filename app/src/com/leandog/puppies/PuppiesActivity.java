781e0299bb99d98304212818d61faf1f85791fae
Levi Wilson <levi@leviwilson.com>
2012-09-26 00:16:21 -0400

    can click to Puppies List

--- app/src/com/leandog/puppies/PuppiesActivity.java
+++ app/src/com/leandog/puppies/PuppiesActivity.java
@@ -1,10 +1,13 @@
 package com.leandog.puppies;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.view.*;
+import android.view.View.OnClickListener;
+
+import com.leandog.puppies.R.id;
 
 public class PuppiesActivity extends Activity {
 
@@ -12,7 +15,8 @@ public class PuppiesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puppies);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(id.view_puppies).setOnClickListener(new OnViewPuppiesListener());
     }
 
     @Override
@@ -21,15 +25,21 @@ public class PuppiesActivity extends Activity {
         return true;
     }
 
-    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private final class OnViewPuppiesListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(PuppiesActivity.this, PuppiesListActivity.class));
+        }
     }
 
 }
